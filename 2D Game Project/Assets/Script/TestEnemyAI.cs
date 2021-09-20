457a3856d9ec215c04264e428fd57df2b9ced451
3A813107 <90887372+3A813107@users.noreply.github.com>
2021-09-20 23:55:27 +0800

    Update TestEnemyAI

--- 2D Game Project/Assets/Script/TestEnemyAI.cs
+++ 2D Game Project/Assets/Script/TestEnemyAI.cs
@@ -2,50 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class TestEnemyAI : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    public Transform leftpoint,rightpoint;
 
     public float Speed;
-    private bool Faceleft;
-    private float leftx,rightx;
+    public List<Transform> points;
+    public int nextID=0;
+
+    int idChangeValue = 1;
+    private PlayerHealth playerHealth;
 
     public int health;
     public int damage;
+
+    private void Reset()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        GetComponent<BoxCollider2D>().isTrigger=true;
+        GameObject root = new GameObject(name+"_Root");
+        root.transform.position =transform.position;
+        transform.SetParent(root.transform);
+        GameObject waypoints = new GameObject("Waypoints");
+        waypoints.transform.SetParent(root.transform);
+        waypoints.transform.position=root.transform.position;
+        GameObject p1 = new GameObject("Point1");p1.transform.SetParent(waypoints.transform);p1.transform.position=root.transform.position;
+        GameObject p2 = new GameObject("Point2");p2.transform.SetParent(waypoints.transform);p2.transform.position=root.transform.position;
+        points = new List<Transform>();
+        points.Add(p1.transform);
+        points.Add(p2.transform);
+    }
     void Start()
     {
-        rb=GetComponent<Rigidbody2D>();
-        leftx=leftpoint.position.x;
-        rightx=rightpoint.position.x;
-        Destroy(leftpoint.gameObject);
-        Destroy(rightpoint.gameObject);
+        playerHealth=GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Movement();
     }
 
     void Movement()
     {
-        if(Faceleft)
+        Transform goalPoint = points[nextID];
+        if(goalPoint.transform.position.x>transform.position.x)
         {
-            rb.velocity=new Vector2(-Speed,rb.velocity.y);
-            if(transform.position.x<leftx)
+            transform.localScale=new Vector3(-3,3,1);
+        }
+        else
+        {
+            transform.localScale=new Vector3(3,3,1);
+        }
+        transform.position = Vector2.MoveTowards(transform.position,goalPoint.position,Speed*Time.deltaTime);
+        if(Vector2.Distance(transform.position,goalPoint.position)<1f)
+        {
+            if(nextID == points.Count-1)
             {
-                transform.localScale=new Vector3(-3,3,1);
-                Faceleft = false;
+                idChangeValue=-1;
             }
-        }else
-        {
-            rb.velocity=new Vector2(Speed,rb.velocity.y);
-            if(transform.position.x>rightx)
+            if(nextID == 0)
             {
-                transform.localScale=new Vector3(3,3,1);
-                Faceleft = true;
+                idChangeValue=1;
+            }
+            nextID += idChangeValue;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            if(playerHealth != null)
+            {
+                playerHealth.DamagePlayer(damage);
             }
         }
     }
+    
 }
