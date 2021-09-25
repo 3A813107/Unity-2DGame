959402dd1011796f4982f0f18b8b9d2579e339a0
3A813107 <90887372+3A813107@users.noreply.github.com>
2021-09-25 23:18:07 +0800

    Add some value

--- 2D Game Project/Assets/Script/GameManager.cs
+++ 2D Game Project/Assets/Script/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
 
     public static bool isPlayerAlive = true;
     public static bool canInpute = true;
+    public static bool GetDoubleJumpAbility=false;
 
     void Awake()
     {
