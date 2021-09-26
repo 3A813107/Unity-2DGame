6a0e7dbaac5d47fe2096892aa7d4e280aae1eb77
3A813107 <90887372+3A813107@users.noreply.github.com>
2021-09-26 16:02:21 +0800

    Fix UI

--- 2D Game Project/Assets/Script/PlayerMana.cs
+++ 2D Game Project/Assets/Script/PlayerMana.cs
@@ -6,6 +6,9 @@ public class PlayerMana : MonoBehaviour
 {
     public int Mana;
     public int MaxMana;
+    public float ManaRegen;//每x秒回1魔
+
+    private float RegenCount;
     void Start()
     {
         Mana=MaxMana; 
@@ -17,6 +20,28 @@ public class PlayerMana : MonoBehaviour
         {
             Debug.Log("Magcal Fire!");
             Mana--;
+        }
+        Regen();
+    }
+
+    void FixedUpdate()
+    {
+  
+    }
+
+    void Regen()
+    {
+        if(Mana < MaxMana)
+        {
+            if(RegenCount >= ManaRegen)
+            {
+                Mana++;
+                RegenCount=0;
+            }
+            else
+            {
+                RegenCount += Time.deltaTime;
+            }
         }
     }
 }
