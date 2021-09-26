70045a49fa238b249452689c201464772f626504
3A813107 <90887372+3A813107@users.noreply.github.com>
2021-09-26 17:41:47 +0800

    Add QuickFall movement

--- 2D Game Project/Assets/Script/PlayerMovement.cs
+++ 2D Game Project/Assets/Script/PlayerMovement.cs
@@ -8,18 +8,27 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     public float MoveSpeed,JumpForce;
-    public Transform GroundCheck;
     public LayerMask Ground;
+
+    public float footOffset;
+    public float GroundDistance=0.2f;
     public bool isGround;
     private bool isJumping;    
-    private bool JumpPressed;
+    private bool QuickFallPressed;
     private float JumpTimeCounter;
     public float JumpTime;
     private bool canDoubleJump;
     private bool FacingRight=true;
 
+    /////////////快速下降//////////////
+    public float QuickFallGravity;
+    public float QuickFallCoolDown;
+    private float LastQuickFall=-10f;
 
-
+    private bool QuickFalling;
+    public float midAirDistance;
+    public bool midAir;
+    /////////////////////////////////////
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
@@ -32,16 +41,18 @@ public class PlayerMovement : MonoBehaviour
         if(GameManager.isPlayerAlive && GameManager.canInpute)
         {
             Jump();
-        }      
+            QuickFallCheak();
+        }
     }
 
     private void FixedUpdate()
     {
         if(GameManager.isPlayerAlive && GameManager.canInpute)
         {
+            PhysicsCheck();
             GroundMovement();
-            isGround = Physics2D.OverlapCircle(GroundCheck.position,0.1f,Ground);
             SwitchAnimation();
+            QuickFall();
         } 
     }
 
@@ -181,6 +192,53 @@ public class PlayerMovement : MonoBehaviour
      
     }
 
+    void QuickFall()
+    {
+        if(QuickFallPressed)
+        {
+            QuickFalling=true;
+            LastQuickFall = Time.time;
+            rb.gravityScale=QuickFallGravity;          
+            if(isGround && QuickFalling)
+            {
+                rb.gravityScale=3;
+                CM_Shake.Instance.ShakeCamera(4f,0.2f);
+                QuickFallPressed=false;
+                QuickFalling=false;
+            }
+        }
+    }
+
+    void QuickFallCheak()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow) && !isGround && Time.time >=(LastQuickFall+QuickFallCoolDown)&&midAir)
+        {
+            QuickFallPressed=true;
+        }
+    }
+    void PhysicsCheck()
+    {
+        RaycastHit2D leftCheck = Raycast(new Vector2(-footOffset,-0.6f),Vector2.down,GroundDistance,Ground);
+        RaycastHit2D rightCheck = Raycast(new Vector2(footOffset,-0.6f),Vector2.down,GroundDistance,Ground);
+        RaycastHit2D midair = Raycast(new Vector2(0,-0.6f),Vector2.down,midAirDistance,Ground);
+        midAir=!midair;
+        if (leftCheck || rightCheck)
+        {
+            isGround = true;
+        }
+        else
+        {
+            isGround=false;
+        }
+    }
+
+    RaycastHit2D Raycast(Vector2 offset,Vector2 rayDiraction,float lenght,LayerMask layer)
+    {
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(pos + offset ,rayDiraction , lenght , layer);
+        Debug.DrawRay(pos + offset , rayDiraction*lenght,Color.blue);
+        return hit;
+    }
 
 }
 
